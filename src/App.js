aeb9219358725a28f397bda2a9a2d215a989fc65
ShadowPrimes <77511645+ShadowPrimes@users.noreply.github.com>
2021-01-15 21:45:09 +0100

    dodali header dodali 4 sekcije koje dovode do 4 razlicite sekcije

--- src/App.js
+++ src/App.js
@@ -4,7 +4,23 @@ import "./styles.css";
 export default function App() {
   return (
     <div>
-      <Logo />
+      <Header />
+      <div id="sekcija1">
+        <p>Sekcija broj 1</p>
+        <Logo />
+      </div>
+      <div id="sekcija2">
+        <p>Sekcija broj 2</p>
+        <Logo />
+      </div>
+      <div id="sekcija3">
+        <p>Sekcija broj 3</p>
+        <Logo />
+      </div>
+      <div id="sekcija4">
+        <p>Sekcija broj 4</p>
+        <Logo />
+      </div>
     </div>
   );
 }
@@ -13,6 +29,17 @@ function Logo() {
   return (
     <div className="babo">
       <h1>ShadowPrimes</h1>
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <div className="header">
+      <a href="#sekcija1">Sekcija1</a>
+      <a href="#sekcija2">Sekcija2</a>
+      <a href="#sekcija3">Sekcija3</a>
+      <a href="#sekcija4">Sekcija4</a>
     </div>
   );
 }
